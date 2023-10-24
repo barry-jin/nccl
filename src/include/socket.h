bd0d9cb006d4a1665286d176ca3b755088b0badf
barry-jin <barryjin1995@gmail.com>
2023-10-23 18:04:25 -0700

    Increase SOCKET timeout

--- src/include/socket.h
+++ src/include/socket.h
@@ -18,7 +18,7 @@
 #define MAX_IFS 16
 #define MAX_IF_NAME_SIZE 16
 #define SLEEP_INT            1000 // connection retry sleep interval in usec
-#define RETRY_REFUSED_TIMES   2e4 // connection refused retry times before reporting a timeout (20 sec)
+#define RETRY_REFUSED_TIMES   2e5 // connection refused retry times before reporting a timeout (20 sec)
 #define RETRY_TIMEDOUT_TIMES    3 // connection timed out retry times (each one can take 20s)
 #define SOCKET_NAME_MAXLEN (NI_MAXHOST+NI_MAXSERV)
 #define NCCL_SOCKET_MAGIC 0x564ab9f2fc4b9d6cULL
